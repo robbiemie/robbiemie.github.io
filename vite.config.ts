5dc1d19db48d70d68d3f20df8cecb57fa5336f11
robbiemie <yang657850144@163.com>
2026-02-28 21:16:00 +0800

    feat: modify public path

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/public/',
+  base: '/',
   publicDir: 'static',
   build: {
-    outDir: 'public',
+    outDir: 'dist',
     emptyOutDir: true
   }
 });
