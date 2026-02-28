e740a582cedbc646de45585040bd3c1472250e01
robbiemie <yang657850144@163.com>
2026-02-28 20:13:55 +0800

    feat: build public assets

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/'
+  base: '/public/',
+  publicDir: 'static',
+  build: {
+    outDir: 'public',
+    emptyOutDir: true
+  }
 });
